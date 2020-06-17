3e18ce1746e5db4608d2d4f4d2574a2bb9b1a12b
Pelin Balci <balci.pelin@gmail.com>
2020-06-17 15:55:11 +0300

    logging

---
+++ SE_Practices/codes/4_logging_ex.py
@@ -0,0 +1,53 @@
+# # Refactor: Wine Quality Analysis
+# In this exercise, you'll refactor code that analyzes a wine quality dataset taken from the UCI Machine Learning
+# Repository [here](https://archive.ics.uci.edu/ml/datasets/wine+quality).
+# Each row contains data on a wine sample, including several physicochemical properties gathered from tests,
+# as well as a quality rating evaluated by wine experts.
+#
+# The code in this notebook first renames the columns of the dataset and then calculates some statistics on how some
+# features may be related to quality ratings. Can you refactor this code to make it more clean and modular?
+
+import pandas as pd
+import numpy as np
+import logging
+
+
+def read_csv():
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    logging.info('Reading csv & Prind fixed acidity')
+    df = pd.read_csv('/Users/pelin.balci/PycharmProjects/aws_ml_foundations/SE_Practices/inputs/winequality-red.csv', sep=';')
+    print(df['fixed acidity'].head())
+    return df
+
+
+# ### Renaming Columns
+def rename_cols(df):
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    logging.info('Renaming the columns')
+    df.columns = [label.replace(' ', '_') for label in df.columns]
+    return df
+
+
+# Create new values:
+def create_new_values(df, column_name):
+    median_value = df[column_name].median()
+    df[column_name] = np.where(df[column_name] >= median_value, 'high', 'low')
+
+
+def main():
+    logging.getLogger().setLevel(logging.DEBUG)
+    logging.info('Started')
+    df = read_csv()
+    df = rename_cols(df)
+    logging.info('Creating new values')
+    for label in df.columns[:-1]:
+        create_new_values(df, label)
+        print(df.groupby(label).quality.mean(), '\n')
+    logging.info('Finished')
+
+
+if __name__ == '__main__':
+    main()
+
+
+logging.debug('Trial: This message should appear on the console')
---
+++ SE_Practices/notes/6_Logging.md
@@ -0,0 +1,51 @@
+# Logging
+
+Logging is valuable for understanding the events that occur while running your program. 
+
+For example, if you run your model over night and see that it's producing ridiculous results the next day, 
+log messages can really help you understand more about the context in which this occurred. Lets learn about 
+the qualities that make a log message effective.
+
+- Be profeesional.
+
+```sh
+Bad: omg not workin!
+Good: Couldn't parse file.
+```
+
+-  Be concise and use normal capitalization
+
+```sh
+Bad: Start Product Recommendation Process
+Good: Generating product recommendations.
+```
+
+- Choose the appropriate level for logging
+
+```sh
+DEBUG - level you would use for anything that happens in the program.
+ERROR - level to record any error that occurs
+INFO - level to record all actions that are user-driven or system specific, such as regularly
+```
+
+- Provide any useful information
+
+```sh
+Bad: Failed to read location data
+Good: Failed to read location data: store_id 8324971
+```
+
+
+Ref: https://docs.python.org/3/howto/logging.html
+
+logging.info() or logging.debug():
+ 
+    Report events that occur during normal operation of a program (e.g. for status monitoring or fault investigation)
+    
+Issue a warning regarding a particular runtime event: logging.warning():
+    
+    if there is nothing the client application can do about the situation, but the event should still be noted
+    
+logging.error(), logging.exception() or logging.critical() as appropriate for the specific error and application domain:
+   
+    Report suppression of an error without raising an exception: 
